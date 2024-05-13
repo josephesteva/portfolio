a910b581427a6b1ae5ee332fc4c6b06416652ead
josephesteva <joe.esteva@gmail.com>
2024-05-13 15:04:44 -0400

    Update project card styling and content

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Project({ project }) {
   return (
     <>
-      <div className="flex m-10 justify-center">
+      <div className="flex m-10 justify-center bg-sky-200">
         <div className="project-card flex flex-col border-2 border-blue-500 p-5 rounded-md shadow-md shadow-sky-300 hover:shadow-lg hover:shadow-sky-500">
           <div className="flex justify-between pb-3 border-b-2 border-blue-300">
-            <h3>{project.title}</h3>
+            <h3 className="text-3xl font-semibold">{project.title}</h3>
             <div>
               <a href={project.siteLink} target="_blank">
                 Check it Out
@@ -17,9 +17,15 @@ export default function Project({ project }) {
             </div>
           </div>
           <div className="flex justify-between p-5">
-            <div className="w-96">
-              <h3 className="text-2xl underline">Description</h3>
-              <p>{project.description}</p>
+            <div className="w-2/3">
+              <div className="mb-5">
+                <h3 className="text-2xl underline">Description</h3>
+                <p>{project.description}</p>
+              </div>
+              <div>
+                <h3 className="text-2xl underline">Tech</h3>
+                <p>{project.description}</p>
+              </div>
             </div>
             <div>
               <h3>Screenshots</h3>
