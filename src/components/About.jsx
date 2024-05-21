e5215e734926f7110360ef0b1be690b06902b913
josephesteva <joe.esteva@gmail.com>
2024-05-20 23:01:16 -0400

    Add mui icon links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function About() {
   return (
@@ -11,11 +13,19 @@ function About() {
       <div id="about" className="flex justify-center mt-10">
         <div className="w-1/2">
           <h3 className="text-2xl font-bold">About</h3>
-          <p>
+          <p className="mb-10">
             I'm a fullstack web developer based in the Washington DC area. I am passionate about finding efficient
             solutions to meaningful problems. I enjoy collaborating with a team to bring exciting applications to life.
             I love to spend time outdoors in my free time and am a fan of skiing, rock climbing, and mountain biking!
           </p>
+          <div className="flex justify-center gap-10">
+            <a href="https://github.com/josephesteva" target="_blank">
+              <GitHubIcon fontSize="large" style={{ transform: "scale(1.5)" }} />
+            </a>
+            <a href="https://www.linkedin.com/in/joseph-esteva" target="_blank">
+              <LinkedInIcon fontSize="large" style={{ transform: "scale(1.7)" }} />
+            </a>
+          </div>
         </div>
       </div>
     </>
