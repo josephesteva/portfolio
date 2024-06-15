b77555297dba25b1079822eed9a87286fe15c470
josephesteva <joe.esteva@gmail.com>
2024-06-14 20:47:15 -0400

    Update data and config settings

--- src/projectData.js
+++ src/projectData.js
@@ -2,8 +2,8 @@ export const projectData = [
 	{
 		id: 'tetross',
 		title: 'TetRoss',
-		description: 'This project was created to practice DOM manipulation using basic HTML, CSS, and JavaScript. The game features block manipulation, scoring, leveling, increasing difficulty, and music',
-		tech: 'HTML, CSS, JavaScript',
+		description: 'A tetris clone with a fun twist, implemented to practice DOM manipulation using basic HTML, CSS, and JavaScript. The game features block manipulation, scoring, leveling, increasing difficulty, music, and variable styling.',
+		tech: 'The basse game board was set up using html and css. The game logic and other functionalities were implemented using Javascript to manipulate the DOM, primarily via updates to CSS as the game progresses.',
 		siteLink: 'https://josephesteva.github.io/tetris/',
 		githubLink: 'https://github.com/josephesteva/tetris',
 		images: [
@@ -26,8 +26,8 @@ export const projectData = [
 	{
 		id: 'deckbuilder',
 		title: 'DeckBuilder',
-		description: "A fullstack Pokemon Trading Card Game deckbuilding and mock social media application. Users can build their own Pokemon Decks, review other users' decks, and post comments. Currently configured to include Gen 1 pokemon cards, the allowable cards can be updated on the backend",
-		tech: 'React, NodeJS, Express, Prisma ORM, CSS, HTML',
+		description: "A fullstack Pokemon Trading Card Game deckbuilding and mock social media application. Users can build their own Pokemon Decks, review other users' decks, and post comments. Originally a group project built at the end of my bootcamp, I have since refactored the codebase, added additional features, and updated styling. Currently configured to include Gen 1 pokemon cards, the allowable cards can be updated on the backend.",
+		tech: 'The front end was built with a system of pages and components using React JS. All styling was done with vanilla CSS in order to practice fundamentals and allow maximum flexibilty. The backend was built with NodeJS, with Express being used for routing and APIs. Prisma ORM was used to set up and interact with a PostgreSQL database.',
 		siteLink: 'https://deckbuilder.onrender.com/',
 		githubLink: 'https://github.com/josephesteva/DeckBuilder',
 		images: [
@@ -49,6 +49,10 @@ export const projectData = [
 			{
 				name: 'NodeJS',
 				path: '/images/icons/nodejs.png'
+			},
+			{
+				name: 'Express',
+				path: '/images/icons/express.png'
 			},
 			{
 				name: 'Prisma',
